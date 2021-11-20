916638ac2ffd11e71096e6e1da49d4ad39f5d27e
Ronald Nettey <ronaldnettey360@gmail.com>
2021-11-20 20:52:45 +0000

    Setting up php

--- web/auth/mailS.php
+++ web/auth/mailS.php
@@ -4,8 +4,7 @@ include("functions.php");
 
 if(isset($_POST["userData"])) {
     $data_received =  $_POST["userData"];
-    print_r($data_received) ;
-    exit(); 
+    echo json_encode(array('success' => 1));
 }
 
 else echo "Couldn't get the right data";
